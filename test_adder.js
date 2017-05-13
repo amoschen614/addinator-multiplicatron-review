f22a4d67ec4a995ad074c16b8efa8a03bb1a4662
Amos Chen <amoschen614@gmail.com>
2017-05-13 15:31:20 -0700

    fix broken test file caused by one typo and one undefined function

--- test_adder.js
+++ test_adder.js
@@ -3,9 +3,14 @@ const add = require('./adder.js');
 class TestSuite {
   runTests() {
     Object.getOwnPropertyNames(Object.getPrototypeOf(this))
-        .filter(prop => this(prop) instanceof Function) //only properties with function values remain
+        .filter(prop => this[prop] instanceof Function) //only properties with function values remain
         .filter(prop => prop.indexOf('test') !== -1)    //the word 'test' is found in function name
         .forEach(testName => this.runTest(testName));   //pass each property value remaining into iterator and call
+  }
+
+  runTest(testName) {
+    const result = this[testName]();
+    console.log(result, testName);
   }
 
   assertEquals(a, b) {
@@ -13,15 +18,15 @@ class TestSuite {
   }
 
   testAddPositiveNumbers() {
-    const result = this.assertEquals(add(5, 7), 12);
+    return this.assertEquals(add(5, 7), 12);
   }
 
   testAddNegativeNumbers() {
-    const result = this.assertEquals(add(-5, -7), -12);
+    return this.assertEquals(add(-5, -7), -12);
   }
 
   testAddMixedNumbers() {
-    const result = this.assertEquals(add(5, -7), -2);
+    return this.assertEquals(add(5, -7), -2);
   }
 }
 
