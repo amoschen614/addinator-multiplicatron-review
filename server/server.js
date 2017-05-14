11a0cab0f2e444b651409f1dee91b710f9f6084e
Amos Chen <amoschen614@gmail.com>
2017-05-13 22:21:50 -0700

    supply default pathname and handle bad requests

--- server/server.js
+++ server/server.js
@@ -5,14 +5,28 @@ const url = require('url');
 const server = http.createServer(function(request, response) {
   
   const urlPath = url.parse(request.url).pathname;
-  const filePath = `.${urlPath}`;
+  let filePath = `./client/${urlPath}`;
 
-  fs.readFile('./client/adder.html', (err, data) => {
-    if (err) {
-      response.end();
-    } else {
-      response.end(data.toString('utf-8'));
+  fs.stat(filePath, function(err, fileInfo) { //nesting results from asynchronicity
+    if (!err && fileInfo.isDirectory()) {
+      filePath += '/index.html';
     }
+  
+    fs.exists(filePath, function(doesExist) {
+      if (!doesExist) {
+        response.statusCode = 404;
+        response.end(`Resource not found: "${urlPath}"`);
+      } else {
+        fs.readFile(filePath, (err, data) => {
+          if (err) {
+            response.statusCode = 500;
+            response.end(`Server error: "${err}"`);
+          } else {
+            response.end(data.toString('utf-8'));
+          }
+        });
+      }
+    });
   });
 });
 
